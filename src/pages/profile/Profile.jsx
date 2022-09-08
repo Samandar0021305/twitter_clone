ef4d1a76a377a979751e7a32e222f040ebfc825a
Samandar0021305 <samandar021325@gmail.com>
2022-09-08 15:48:59 +0500

    Initialize project using Create React App

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -2,11 +2,67 @@ import Saidbar from "../../components/saidbar/Saidbar"
 
 import "./profile.scss";
 
+
+// img
+import twites from "../../img/twites.png";
+
+import Group from "../../img/Group.png";
+import Group1 from "../../img/Group1.png";
+import Group2 from "../../img/Group2.png";
+import Group3 from "../../img/Group3.png";
+
+
+
 function Profile(){
     return(
         <div className="profile">
             <Saidbar />
              
+
+             <div className="profile-center">
+                   <div className="profile-center-topbar">
+                     <img src={twites} alt="" />
+                    </div> 
+
+                    <div className="profile-center-background"></div>
+
+                    <div className="profile-center-box">
+                        <button className="profile-center-box-btn">Edit profile</button>
+                       <div className="profile-center-box-title">
+                       <h2>Bobur</h2>
+                        <span>@bobur_mavlonov</span>
+                        <p>UX&UI designer at  <b>@abutechuz</b></p>
+                       </div>
+
+                       <ul className="profile-center-box-list">
+                        <li>
+                            <img src={Group} alt="" />
+                            <p>Mashag’daman</p>
+                        </li>
+                        <li>
+                            <img src={Group1} alt="" />
+                            <p>t.me/boburjon_mavlonov</p>
+                        </li>
+                        <li>
+                            <img src={Group2} alt="" />
+                            <p>Born November 24, 2000</p>
+                        </li>
+                        <li>
+                            <img src={Group3} alt="" />
+                            <p>Joined May 2020</p>
+                        </li>
+                       </ul>
+                    </div>
+
+             
+             </div>
+
+
+
+        <div className="profile-right">
+
+        </div>
+
         </div>
     )
 }
